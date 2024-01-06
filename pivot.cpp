a6e68a83da9f532d311bb20fabb942040e755bf5
sehajs5 <125692554+sehajs5@users.noreply.github.com>
2024-01-07 02:09:06 +0530

    Add files via upload

---
+++ pivot.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int getPivot(int arr[], int size){
+    int s=0;
+    int e=size-1;
+    int mid= s+(e-s)/2;
+    while(s<e){
+        if(arr[mid]>=arr[0]){
+            s=mid+1;
+        }
+        else{
+            e=mid;
+        }
+        mid=s+(e-s)/2;
+    }
+    return s;
+}
+int main(){
+    int arr[5]={3,8,10,17,1};
+    // int size=sizeof(arr)/sizeof(int);
+    cout<<getPivot(arr,5);
+}
