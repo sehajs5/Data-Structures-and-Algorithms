9eaff50c5f87c7c53a9f31e58ead17cd94645fcf
sehajs5 <125692554+sehajs5@users.noreply.github.com>
2024-01-12 23:57:56 +0530

    Add files via upload

---
+++ list.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<list>
+//using doubly linked list
+//cannot randomly excess - O(n)
+
+using namespace std;
+int main(){
+    list<int> l;
+    l.push_back(1);
+    l.pop_front(2);
+    //push_back, pop_front, size
+    //copying old list
+    //list<int> n(l)
+}
