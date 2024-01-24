a34eff9ec2b38302aa2034d1c6c7e0ecdd5c4461
sehajs5 <125692554+sehajs5@users.noreply.github.com>
2024-01-24 23:37:13 +0530

    Add files via upload

---
+++ wavePrint.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+vector<int> wavePrint(vector<vector<int>> arr, int nRows, int mCols)
+{
+    //Write your code here
+    vector <int> ans;
+    for(int i=0;i<mCols;i++){
+        if(i%2==0){
+            for (int j = 0; j < nRows; j++)
+            {
+                ans.push_back(arr[j][i]);
+            }
+        }
+        
+        else{
+            for (int j = nRows - 1; j >= 0; j--)
+            {
+                ans.push_back(arr[j][i]);
+            }
+        }
+        
+    }
+    return ans;
+}
