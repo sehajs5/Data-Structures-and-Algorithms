07986e9b9947d92fad48529d25fd76739b2d4c0d
sehajs5 <125692554+sehajs5@users.noreply.github.com>
2024-02-05 23:28:32 +0530

    Add files via upload

---
+++ macros.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+#define Pi 3.14
+//macro- piece of program that is replaced by a value
+int main(){
+    int radius= 5;
+    int area= Pi*2*radius;
+    cout<<area;
+}
