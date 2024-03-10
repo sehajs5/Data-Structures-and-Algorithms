006116e79ea711ed178a748deb0db29280eb8325
sehajs5 <125692554+sehajs5@users.noreply.github.com>
2024-03-10 22:51:21 +0530

    Add files via upload

--- permutation.cpp
+++ permutation.cpp
@@ -1,73 +1,34 @@
 #include<iostream>
 #include<vector>
 using namespace std;
-    bool areVectorsEqual(vector<int>a, vector<int>b){
-        for(int i=0;i<26;i++){
-            if(a[i]!=b[i]){
-                return false;
-            }
+    void solve(vector<int> nums, vector<vector<int> > &result,  int s, int n){
+        if(s>=n){
+            result.push_back(nums);
+            return;
         }
-        return true;
-    }
-bool checkInclusionBool(string s1, string s2) {
-        if(s2.size()<s1.size()) return false;
-        vector<int> freqS1(26,0);
-        for(char c:s1){
-            freqS1[c-'a']++;
-        }
-        vector <int> freqS2(26,0);
-        int i=0,j=0;
-        while(j<s2.size()){
-            freqS2[s2[j]-'a']++;
-            if(j-i+1==s1.size()){
-                if(areVectorsEqual(freqS1,freqS2))
-                return true;
-            }
-            if(j-i+1<s1.size()){
-                j++;
-            }
-            else{
-                freqS2[s2[i]-'a']--;
-                i++;
-                j++;
-            }
-        }
-        
-        return false;
-    }
-
-    vector<int> findAnagrams(string s, string p) {
-        vector<int> a;
-        if(s.size()<p.size()) return a;
-        vector <int> countp(26,0);
-        for(int i=0;i<p.size();i++){
-            int index=p[i]-'a';
-            countp[index]++;
-        }
-        int i=0,j=0;
-        vector<int> counts(26,0);
-        while(j<s.size()){
-            counts[s[j]-'a']++;
-            if(j-i+1==p.size()){
-                if(areVectorsEqual(counts,countp)){
-                    a.push_back(i);
-                } 
-            }
-            if(j-i+1<p.size()){
-                j++;
-            }
-            else{
-                counts[s[i]-'a']--;
-                j++;
-                i++;
-            }
-        }
-        for(int i=0;i<a.size();i++){
-            cout<<a[i]<<endl;
+        for(int i=s;i<n;i++){
+            swap(nums[i],nums[s]);
+            solve(nums, result, s+1, n);
+            swap(nums[i],nums[s]);
         }
     }
-int main(){
-    string s1= "ab";
-    string s2= "eidbaooo";
-    cout<<checkInclusionBool(s1,s2)<<endl;
-}
+    vector<vector<int> > permute(vector<int>& nums) {
+        vector<vector<int> > result;
+        int n= nums.size();
+        int s=0;
+        solve(nums, result, s,n);
+        return result; 
+    }
+    int main(){
+        vector<int> nums;
+        nums.push_back(1);
+        nums.push_back(2);
+        nums.push_back(3);
+        vector<vector<int> > ans;
+        ans= permute(nums);
+        for(int i=0;i<ans.size();i++){
+            for(int j=0;j<ans.size();j++){
+                cout<<ans[i][j];
+            }
+        }
+    }
